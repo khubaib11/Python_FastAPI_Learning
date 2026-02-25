0b95cb143a94026a174352e2945bfe7815e62f5d
khubaib11 <khankhubaib089@gmail.com>
2026-02-25 22:08:03 +0500

    Day 1 Python Basics

---
+++ Day1/controlflow.py
@@ -0,0 +1,73 @@
+# for i in (1,2,3,4,5,7):
+#     print(i)
+
+
+# a=["abs",2,3,"check"]
+
+# for i in a:
+#     print(i)
+
+# a=10
+# while a>5:
+#     a-=1
+#     if a == 8:
+#         continue
+#     print(a)
+
+
+# try:
+#     print("Hello")
+# except:
+#     print("Error occurred")
+# else:
+#     print("No errors!")
+
+
+
+# for i in range(5,1,-2):
+#     print(i)
+
+
+# print(range(5,1))
+
+# for i in range(5,1,-1):
+#     print(i)
+
+
+
+
+# x = 5 
+# def outer():
+#     y = 10 
+#     def inner():
+#         nonlocal y
+#         global x
+#         x += 5
+#         y += 5
+#         z = 15  
+#         print(x, y, z)
+
+#     inner()
+#     print(x, y)
+
+# outer()
+# print(x)  
+
+
+
+# setVal={55,44,3,1,2}
+
+# for i in range(len(setVal)):
+#     print(setVal[i])
+
+
+# print(setVal)
+
+# myList = [10, 11, 13, 14, 15]
+
+# for i in range(len(myList)):
+#     if myList[i] == 13:
+#         myList[i] = 12   # change the value at index
+#     print(myList[i])
+
+# print(myList)
